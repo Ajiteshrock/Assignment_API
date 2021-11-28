90f964367f4d941bdc65b2bef91e00923126bea1
Ajitesh Mishra <47673623+Ajiteshrock@users.noreply.github.com>
2021-11-29 01:48:09 +0530

    all

--- movies/settings.py
+++ movies/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #JWT Toekn Authentication
 from datetime import timedelta
 SIMPLE_JWT = {
