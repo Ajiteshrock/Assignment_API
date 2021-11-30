37f8cb1c85dc2499c6ff1ca67c32a3807dee99df
Ajitesh Mishra <47673623+Ajiteshrock@users.noreply.github.com>
2021-11-30 12:51:34 +0530

    Update views.py

--- demoapi/views.py
+++ demoapi/views.py
@@ -6,7 +6,6 @@ from rest_framework.generics import RetrieveAPIView , ListAPIView , CreateAPIVie
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny ,IsAuthenticated , IsAdminUser
 from rest_framework.views import APIView
-# from .serializers import MyTokenObtainPairSerializerStudent, StudentSerializser
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated , IsAdminUser
 from . import models
